867bc7f558417c69a77cc166f6f29f723b1ffdd1
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-09-09 23:04:19 +0500

    added dynamic routing

--- components/app/dashboard/user/page.tsx
+++ components/app/dashboard/user/page.tsx
@@ -9,10 +9,7 @@ import Link from 'next/link'
 function User() {
   return (
     <>
-    <Link href = "user/1">User 1</Link>
-    <Link href = "user/2">User 2</Link>
-    <Link href = "user/3">User 3</Link>
-    <Link href = "user/4">User 4</Link>
+  
     </>
   )
 }
